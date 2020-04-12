562e7b719dcce6f9d21220c737b30311d80ebcbc
Deniska Kiryanov <denisich2001@mail.ru>
2020-04-13 00:36:58 +0400

    selection of exploring library

--- parser.php
+++ parser.php
@@ -1,1 +1,9 @@
-"<?php  ?>"  
+<?php  
+    #$ch = curl_init('https://www.yandex.com');
+    #curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    #curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    #$b = curl_exec($ch);
+    $ch = file_get_contents('https://www.yandex.com');
+    #print_r($ch);
+    echo $ch;
+?>  
